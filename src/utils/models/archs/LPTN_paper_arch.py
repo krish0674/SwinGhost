da4655a3f9fdee5eefb3fae641b39ec4483aa5a0
krish0674 <118186942+krish0674@users.noreply.github.com>
2025-04-13 13:30:10 +0530

    hi

--- src/utils/models/archs/LPTN_paper_arch.py
+++ src/utils/models/archs/LPTN_paper_arch.py
@@ -127,7 +127,7 @@ class WindowTransformerBlock2D(nn.Module):
     def forward(self, x):
         B, C, H, W = x.shape
         # H, W = x.shape[2], x.shape[3]
-        assert (int(H), int(W)) == self.resolution, f"Expected {self.resolution}, got {(H, W)}"
+        # assert (int(H), int(W)) == self.resolution, f"Expected {self.resolution}, got {(H, W)}"
         x_ = x.permute(0, 2, 3, 1).contiguous().view(B, H * W, C)  # (B, H*W, C)
         x_ = self.attn(self.norm(x_))
         x_ = x_.view(B, H, W, C).permute(0, 3, 1, 2)  # back to (B, C, H, W)
